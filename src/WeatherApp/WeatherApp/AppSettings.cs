21b89df816dc1be6f5d97099f2479968bcc474af
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-09-14 13:52:30 +0200

    Added source code

---
+++ src/WeatherApp/WeatherApp/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherApp
+{
+    public class AppSettings
+    {
+        public static string ApiKey = "INSERT YOUR API KEY";    // More info: https://openweathermap.org/appid
+        public static bool IsImperial = false;
+    }
+}
---
+++ src/WeatherApp/WeatherApp/Converters/BackgroundConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace WeatherApp.Converters
+{
+    public class BackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return string.Empty;
+
+            var condition = value.ToString();
+
+            if (condition.Contains("cloud") || condition.Contains("haze"))
+            {
+                if (Device.RuntimePlatform == Device.UWP)
+                    return "Assets/clouds-background.jpg";
+                else
+                    return "clouds_background.jpg";
+            }
+            else if (condition.Contains("rain"))
+            {
+                if (Device.RuntimePlatform == Device.UWP)
+                    return "Assets/rain-background.jpg";
+                else
+                    return "rain_background.jpg";
+            }
+            else if (condition.Contains("sun") || (condition.Contains("clear sky")))
+            {
+                if (Device.RuntimePlatform == Device.UWP)
+                    return "Assets/sun-background.jpg";
+                else
+                    return "sun_background.jpg";
+            }
+            else
+                return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
